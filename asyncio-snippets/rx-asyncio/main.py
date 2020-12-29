13315fd0f0263d64706e8d4b8c6cce68ec301815
pen-dev <pelipenkovladislav@gmail.com>
2020-12-29 20:15:26 +0300

    черновик

--- asyncio-snippets/rx-asyncio/main.py
+++ asyncio-snippets/rx-asyncio/main.py
@@ -1,70 +1,59 @@
 from tools.future import Future
+from typing import Callable
 import asyncio
+import aioreactive as rx
+from aioreactive import AsyncObservable as Stream
+
+
+async def sum_stream() -> Future[None]:
+    f = asyncio.get_running_loop().create_future()
+    f.add_done_callback(lambda: print('Hello'))
+    return asyncio.sleep(2, f)
 
 
 
-def fetch_user_order() -> Future[str]:
-    return asyncio.sleep(4, 'Large Latte')
+def count_stream(to: int) -> Stream[int]:
+    return rx.from_iterable(range(to))
 
-
-# def create_call_later(loop, i: int, future, ):
-#     loop.call_later(i, f, future)
-
-def count_seconds(s: int):
-    return (
-        asyncio.sleep(i, i)
-        for i in range(s)
-    )
 
 async def main():
-    # # Отработают по очереди (Синхронный запуск)
-    # await fetch_user_order(),
-    # await print_text()
+    await sum_stream()
 
-    # Асинхронный запуск (Вариант 1)
-
-    f = await asyncio.gather(
-        fetch_user_order(),
-        *count_seconds(10)
-    )
-
-    [print(i) for i in f]
-
-    # Асинхронный запуск (Вариант 2)
-    # task1 = asyncio.create_task(fetch_user_order())
-    # task2 = asyncio.create_task(print_text())
-    #
-    # await task1
-    # await task2
 
 asyncio.run(main())
 
 # output:
-# Fetching user order...
-# Exception: Logout failed: user ID is invalid
+# 45
 
 
 
 
-# Future<void> printOrderMessage() async {
-#   print('Awaiting user order...');
-#   var order = await fetchUserOrder();
-#   print('Your order is: $order');
+# import 'dart:async';
+#
+# Future<int> sumStream(Stream<int> stream) async {
+#   var sum = 0;
+#   await for (var value in stream) {
+#     sum += value;
+#   }
+#   return sum;
 # }
 #
-# Future<String> fetchUserOrder() {
-#   // Imagine that this function is more complex and slow.
-#   return Future.delayed(Duration(seconds: 4), () => 'Large Latte');
-# }
-#
-# Future<void> main() async {
-#   countSeconds(4);
-#   await printOrderMessage();
-# }
-#
-# // You can ignore this function - it's here to visualize delay time in this example.
-# void countSeconds(int s) {
-#   for (var i = 1; i <= s; i++) {
-#     Future.delayed(Duration(seconds: i), () => print(i));
+# Stream<int> countStream(int to) async* {
+#   for (int i = 1; i <= to; i++) {
+#     yield i;
 #   }
 # }
+#
+# main() async {
+#   var stream = countStream(10);
+#   var sum = sumStream(stream);
+#   print(await sum); // 55
+# }
+#
+
+
+
+
+
+
+
